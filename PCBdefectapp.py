23e82fbc00d8855fdb06742f0d417224692fc72a
coresys-ai <129962498+coresys-ai@users.noreply.github.com>
2023-04-21 14:26:39 +0100

    Update PCBdefectapp.py

--- PCBdefectapp.py
+++ PCBdefectapp.py
@@ -52,7 +52,7 @@ def imageInput(src):
         with col2:            
             if image_file is not None and submit:
                 #call Model prediction--
-                model = torch.hub.load('ultralytics/yolov5', 'custom', path= yolov5s.pt', force_reload=True) 
+                model = torch.hub.load('ultralytics/yolov5', 'custom', path= 'yolov5s.pt', force_reload=True) 
                 pred = model(image_file)
                 pred.render()  # render bbox in image
                 for im in pred.ims:
