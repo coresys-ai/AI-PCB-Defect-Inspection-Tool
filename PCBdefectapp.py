da2c943f74ea553bd6a4b32ebf3f105023707f29
coresys-ai <129962498+coresys-ai@users.noreply.github.com>
2023-04-21 14:53:01 +0100

    Update PCBdefectapp.py

--- PCBdefectapp.py
+++ PCBdefectapp.py
@@ -13,6 +13,7 @@ def imageInput(src):
     
     if src == 'Upload your own PCB Image':
         image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+        submit = st.button("Predict PCB Defect!")
         col1, col2 = st.columns(2)
         if image_file is not None:
             img = Image.open(image_file)
