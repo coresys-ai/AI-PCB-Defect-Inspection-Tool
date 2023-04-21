4b5cde2792562847f0acbcb990ac9a08f7a0bacf
coresys-ai <129962498+coresys-ai@users.noreply.github.com>
2023-04-21 12:05:41 +0100

    Update PCBdefectapp.py

--- PCBdefectapp.py
+++ PCBdefectapp.py
@@ -1,54 +1,123 @@
 import streamlit as st
 import torch
-import cv2
-import numpy as np
+from detect import detect
 from PIL import Image
+from io import *
+import glob
+from datetime import datetime
+import os
+import wget
+import time
 
-# Load the YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+def imageInput(device, src):
+    
+    if src == 'Upload your own data.':
+        image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='Uploaded Image', use_column_width='always')
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
 
-# Define the inference function
-def detect_pcb(image):
-    # Convert the image to a numpy array
-    img = np.array(image.convert('RGB'))
+            #call Model prediction--
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yoloTrained.pt', force_reload=True) 
+            model.cuda() if device == 'cuda' else model.cpu()
+            pred = model(imgpath)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(outputpath)
 
-    # Perform inference on the image
-    results = model(img)
+            #--Display predicton
+            
+            img_ = Image.open(outputpath)
+            with col2:
+                st.image(img_, caption='Model Prediction(s)', use_column_width='always')
 
-    # Get the predicted bounding boxes and labels
-    bboxes = results.xyxy[0].cpu().numpy()
-    labels = results.names[results.xyxy[0][:, -1].long().cpu()]
+    elif src == 'From test set.': 
+        # Image selector slider
+        imgpath = glob.glob('data/images/*')
+        imgsel = st.slider('Select random images from test set.', min_value=1, max_value=len(imgpath), step=1) 
+        image_file = imgpath[imgsel-1]
+        submit = st.button("Predict!")
+        col1, col2 = st.columns(2)
+        with col1:
+            img = Image.open(image_file)
+            st.image(img, caption='Selected Image', use_column_width='always')
+        with col2:            
+            if image_file is not None and submit:
+                #call Model prediction--
+                model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yoloTrained.pt', force_reload=True) 
+                pred = model(image_file)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                #--Display predicton
+                    img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                    st.image(img_, caption='Model Prediction(s)')
 
-    # Draw the predicted bounding boxes and labels on the image
-    for bbox, label in zip(bboxes, labels):
-        x1, y1, x2, y2 = bbox.astype(np.int)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    # Convert the image back to a PIL image
-    output_image = Image.fromarray(img)
 
-    return output_image
 
-# Define the Streamlit app
-def app():
-    # Set the app title
-    st.title('YOLOv5 PCB Detection')
+def videoInput(device, src):
+    uploaded_video = st.file_uploader("Upload Video", type=['mp4', 'mpeg', 'mov'])
+    if uploaded_video != None:
 
-    # Create an input file uploader
-    uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
+        ts = datetime.timestamp(datetime.now())
+        imgpath = os.path.join('data/uploads', str(ts)+uploaded_video.name)
+        outputpath = os.path.join('data/video_output', os.path.basename(imgpath))
 
-    # Perform inference when the user uploads an image
-    if uploaded_file is not None:
-        # Read the image
-        image = Image.open(uploaded_file)
+        with open(imgpath, mode='wb') as f:
+            f.write(uploaded_video.read())  # save video to disk
 
-        # Perform object detection using the YOLOv5 model
-        output_image = detect_pcb(image)
+        st_video = open(imgpath, 'rb')
+        video_bytes = st_video.read()
+        st.video(video_bytes)
+        st.write("Uploaded Video")
+        detect(weights="models/yoloTrained.pt", source=imgpath, device=0) if device == 'cuda' else detect(weights="models/yoloTrained.pt", source=imgpath, device='cpu')
+        st_video2 = open(outputpath, 'rb')
+        video_bytes2 = st_video2.read()
+        st.video(video_bytes2)
+        st.write("Model Prediction")
 
-        # Display the output image with the predicted bounding boxes and labels
-        st.image(output_image, caption='Output', use_column_width=True)
 
-# Run the app
+def main():
+    # -- Sidebar
+    st.sidebar.title('⚙️Options')
+    datasrc = st.sidebar.radio("Select input source.", ['From test set.', 'Upload your own data.'])
+    
+        
+                
+    option = st.sidebar.radio("Select input type.", ['Image', 'Video'], disabled = True)
+    if torch.cuda.is_available():
+        deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = False, index=1)
+    else:
+        deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = True, index=0)
+    # -- End of Sidebar
+
+    st.header('📦 AI PCB Defect Detection Tool ')
+    st.subheader('👈🏽 Select the options')
+    #st.sidebar.markdown("https://github.com/thepbordin/Obstacle-Detection-for-Blind-people-Deployment")
+    if option == "Image":    
+        imageInput(deviceoption, datasrc)
+    elif option == "Video": 
+        videoInput(deviceoption, datasrc)
+
+    
+
 if __name__ == '__main__':
-    app()
+  
+    main()
+@st.cache
+def loadModel():
+    start_dl = time.time()
+    model_file = wget.download('https://archive.org/download/yoloTrained/yoloTrained.pt', out="models/")
+    finished_dl = time.time()
+    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
+loadModel()
