278eb4be23fb4bd259045c79c89f2b3bfe28efe6
coresys-ai <129962498+coresys-ai@users.noreply.github.com>
2023-04-21 14:12:06 +0100

    Update PCBdefectapp.py

--- PCBdefectapp.py
+++ PCBdefectapp.py
@@ -25,7 +25,7 @@ def imageInput(src):
                 f.write(image_file.getbuffer())
 
             #call Model prediction--
-            model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', force_reload=True) 
+            model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) 
             #model.cuda() if device == 'cuda' else model.cpu()
             pred = model(imgpath)
             pred.render()  # render bbox in image
@@ -52,7 +52,7 @@ def imageInput(src):
         with col2:            
             if image_file is not None and submit:
                 #call Model prediction--
-                model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5.pt', force_reload=True) 
+                model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) 
                 pred = model(image_file)
                 pred.render()  # render bbox in image
                 for im in pred.ims:
