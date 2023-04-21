cd7b2a8779f945cfe1aa43176855c277f058ba1b
coresys-ai <129962498+coresys-ai@users.noreply.github.com>
2023-04-21 16:35:34 +0100

    Update PCBdefectapp.py

--- PCBdefectapp.py
+++ PCBdefectapp.py
@@ -64,9 +64,9 @@ def imageInput(src):
                     st.image(img_, caption='AI Defect PredictionS')
 
 def main():
-    st.set_page_config(page_title ="GlobalHRIS", page_icon =":guardsman:", layout ="wide")
-    st.image("logo.bmp", width = 300)
-    st.title("Global HR Implementation Services Limited")
+    st.set_page_config(page_title ="CORESYS", page_icon =":guardsman:", layout ="wide")
+    st.image("logo.jpg", width = 300)
+    st.title("Coresys Limited")
     st.header('AI Tool for PCB Defect Detection')
     st.subheader('👈🏽 Select the Image Source options')
     st.sidebar.title('⚙️Options')
